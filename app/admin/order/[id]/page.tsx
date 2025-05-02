e41b5270262a0019c5dfe240498da48b6dc6efaa
giladfuchs <igilfu@gmail.com>
2025-05-02 16:00:34 +0300

    work order view

--- app/admin/order/[id]/page.tsx
+++ app/admin/order/[id]/page.tsx
@@ -1,8 +1,24 @@
 import {notFound} from 'next/navigation';
-import {Box, Divider, Grid, Typography} from '@mui/material';
 import Price from 'components/price';
 import {getOrderById} from "../../../../lib/api";
 
+import {
+    Box,
+    Divider,
+    Grid,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+
+    Typography
+} from '@mui/material';
+import PersonTwoToneIcon from '@mui/icons-material/PersonTwoTone';
+import MailTwoToneIcon from '@mui/icons-material/MailTwoTone';
+import PhonelinkRingTwoToneIcon from '@mui/icons-material/PhonelinkRingTwoTone';
+import AccessTimeTwoToneIcon from '@mui/icons-material/AccessTimeTwoTone';
+import MonetizationOnTwoToneIcon from '@mui/icons-material/MonetizationOnTwoTone';
+import ShoppingBagTwoToneIcon from '@mui/icons-material/ShoppingBagTwoTone';
 
 export default async function OrderViewPage({
                                                 params,
@@ -19,23 +35,90 @@ export default async function OrderViewPage({
             <Typography variant="h4" fontWeight="bold" textAlign="center" mb={3}>
                 הזמנה #{order.id}
             </Typography>
+            <Grid container justifyContent="center" sx={{ direction: 'rtl', mb: 4 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <List component="nav" disablePadding>
+                                <ListItem disableGutters>
+                                    <ListItemIcon sx={{ minWidth: 40 }}>
+                                        <PersonTwoToneIcon sx={{ fontSize: '1.3rem' }} />
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={<Typography variant="subtitle1">שם מלא</Typography>}
+                                        secondary={<Typography variant="body2">{order.name}</Typography>}
+                                        sx={{ textAlign: 'right' }}
+                                    />
+                                </ListItem>
 
-            <Grid container spacing={2} mb={2}>
-                <Grid item xs={12} sm={6}>
-                    <Typography variant="subtitle1">👤 {order.name}</Typography>
-                    <Typography variant="body2">📧 {order.email}</Typography>
-                    <Typography variant="body2">📱 {order.phone}</Typography>
-                </Grid>
-                <Grid item xs={12} sm={6} textAlign="left">
-                    <Typography variant="body2">
-                        🕒 {new Date(order.createdAt).toLocaleString('he-IL')}
-                    </Typography>
-                    <Typography variant="body2">🧺 כמות: {order.totalQuantity}</Typography>
-                    <Typography variant="body2">
-                        💰 סה"כ: <Price amount={order.cost}/>
-                    </Typography>
-                </Grid>
+                                <ListItem disableGutters>
+                                    <ListItemIcon sx={{ minWidth: 40 }}>
+                                        <MailTwoToneIcon sx={{ fontSize: '1.3rem' }} />
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={<Typography variant="subtitle1">אימייל</Typography>}
+                                        secondary={<Typography variant="body2">{order.email}</Typography>}
+                                        sx={{ textAlign: 'right' }}
+                                    />
+                                </ListItem>
+
+                                <ListItem disableGutters>
+                                    <ListItemIcon sx={{ minWidth: 40 }}>
+                                        <PhonelinkRingTwoToneIcon sx={{ fontSize: '1.3rem' }} />
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={<Typography variant="subtitle1">טלפון</Typography>}
+                                        secondary={<Typography variant="body2">{order.phone}</Typography>}
+                                        sx={{ textAlign: 'right' }}
+                                    />
+                                </ListItem>
+                            </List>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <List component="nav" disablePadding>
+                                <ListItem disableGutters>
+                                    <ListItemIcon sx={{ minWidth: 40 }}>
+                                        <AccessTimeTwoToneIcon sx={{ fontSize: '1.3rem' }} />
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={<Typography variant="subtitle1">תאריך יצירה</Typography>}
+                                        secondary={
+                                            <Typography variant="body2">
+                                                {new Date(order.createdAt).toLocaleDateString('he-IL')}
+                                            </Typography>
+                                        }
+                                        sx={{ textAlign: 'right' }}
+                                    />
+                                </ListItem>
+
+                                <ListItem disableGutters>
+                                    <ListItemIcon sx={{ minWidth: 40 }}>
+                                        <ShoppingBagTwoToneIcon sx={{ fontSize: '1.3rem' }} />
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={<Typography variant="subtitle1">כמות מוצרים</Typography>}
+                                        secondary={
+                                            <Typography variant="body2">{order.totalQuantity}</Typography>
+                                        }
+                                        sx={{ textAlign: 'right' }}
+                                    />
+                                </ListItem>
+
+                                <ListItem disableGutters>
+                                    <ListItemIcon sx={{ minWidth: 40 }}>
+                                        <MonetizationOnTwoToneIcon sx={{ fontSize: '1.3rem' }} />
+                                    </ListItemIcon>
+                                    <ListItemText
+                                        primary={<Typography variant="subtitle1">סה״כ לתשלום</Typography>}
+                                        secondary={<Price amount={order.cost} />}
+                                        sx={{ textAlign: 'right' }}
+                                    />
+                                </ListItem>
+                            </List>
+                        </Grid>
+                    </Grid>
             </Grid>
+
 
             <Divider sx={{my: 2}}/>
 
