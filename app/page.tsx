9f40906a7a99fc54eaa27c51ab4cf20f0a128092
giladfuchs <igilfu@gmail.com>
2025-05-02 14:40:43 +0300

    work on new domain

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
-import { Suspense } from "react";
+import {Suspense} from "react";
 import SearchLayout from "./collection/layout";
 import CollectionPage from "./collection/page";
 
 export const metadata = {
-  description: "חנות שתילים של יערה",
-  openGraph: {
-    type: "website",
-  },
+    title: 'יערה - הבית של הצמחים',
+    description: 'צמחים ביתיים, מתנות ירוקות ומוצרים ייחודיים באהבה.',
+    openGraph: {
+        title: 'יערה - הבית של הצמחים',
+        description: 'צמחים ביתיים, מתנות ירוקות ומוצרים ייחודיים באהבה.',
+        images: ['https://scontent.ftlv6-1.fna.fbcdn.net/v/t39.30808-1/366316342_594394206200903_864814361091134817_n.png?stp=dst-png_s480x480&_nc_cat=103&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=mxpdzxGXF2QQ7kNvwEDiinH&_nc_oc=AdncL-ob6VBUU90XJb183Qi2ViCNBLQ1x1rgNtgutNz53YcGzsDqB5MvqiqN1FqBq48&_nc_zt=24&_nc_ht=scontent.ftlv6-1.fna&_nc_gid=LmVmTwziCIs1MWu_bn3G3w&oh=00_AfGs8tmANM3zIGq9G46NOS8aGeElrnu6BXGX1zWJmikqhA&oe=681A7206'],
+        url: 'https://yaara-store.vercel.app',
+        type: "website",
+
+    }
+
 };
 
 export default async function HomePage() {
-  return (
-    <SearchLayout>
-      <Suspense fallback={null}>
-        <CollectionPage />
-      </Suspense>
-    </SearchLayout>
-  );
+    return (
+        <SearchLayout>
+            <Suspense fallback={null}>
+                <CollectionPage/>
+            </Suspense>
+        </SearchLayout>
+    );
 }
