158cbef887b982c3bc5b31c3a2a2891a3252a913
giladfuchs <igilfu@gmail.com>
2025-05-16 12:55:49 +0300

    safeDecodeURIComponent1

--- app/category/[category]/page.tsx
+++ app/category/[category]/page.tsx
@@ -9,7 +9,7 @@ import {
   getCategoryDescription,
 } from "lib/assets/i18n/seo_heb";
 import { Product } from "lib/types";
-import {safeDecodeURIComponent} from "../../../lib/helper";
+import {safeDecodeURIComponent} from "lib/helper";
 
 const ClientProduct = dynamic(
   () => import("components/product/ClientProduct"),
